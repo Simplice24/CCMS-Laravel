5c6c2b4734837d54f31a20a4eb004941b153a69c
Simplice NIYONZIMA <89002414+Simplice24@users.noreply.github.com>
2023-03-03 02:11:30 -0800

    first commit

--- resources/views/Customized/Disease-details.blade.php
+++ resources/views/Customized/Disease-details.blade.php
@@ -154,12 +154,7 @@
               <div class="card">
                 <div class="card-body">
                   <div class="d-flex flex-column align-items-center text-center">
-                    <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" class="rounded-circle" width="150">
-                    <div class="mt-3">
-                      <h4>{{$diseaseinfo->disease_name}}</h4>
-                      <h5>{{$diseaseinfo->category}}</h5>
-                      <h5>{{$diseaseinfo->description}}</h5>
-                    </div>
+                    <img src="{{asset('/storage/images/diseases/'.$diseaseinfo->image)}}" alt="Admin"  width="360x" >
                   </div>
                 </div>
               </div>
